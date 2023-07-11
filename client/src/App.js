9c385fb4d0bd7761d78f84dc76c05c1b425b148e
eethanlau <ethan.lau151@gmail.com>
2023-07-11 15:28:00 -0400

    Start project

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <span>Home</span>
+  },
+  {
+    path: "/products/:id",
+    element: <span>Category</span>
+  },
+  {
+    path: "/product/:id",
+    element: <span>Product</span>
+  }
+])
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  );
+}
+
+export default App;
