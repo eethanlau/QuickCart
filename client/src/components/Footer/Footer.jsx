eafe73c29b5729a286d637c600109d78f9711c1a
eethanlau <ethan.lau151@gmail.com>
2023-07-13 22:45:24 -0400

    complete footer design

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Footer.scss'
 
 const Footer = () => {
   return (
@@ -21,14 +22,29 @@ const Footer = () => {
           <span>Cookies</span>
         </div>
         <div className="item">
-
+          <h1>About</h1>
+          <span>
+            Welcome to QuickCart! Our store offers a variety of clothing in various colour palettes and styles.
+          </span>
         </div>
         <div className="item">
-          
-        </div>
+          <h1>Contact</h1>
+          <span>
+            Our contact information is listed below. Feel free to contact us for any inquiries.
+          </span>
+        </div> 
       </div>
       <div className="bottom">
+        <div className="left">
+          <span className="logo">QuickCart</span>
+          <span className="copyright">
+            © Copyright 2023. All Rights Reserved
+          </span>
+        </div>
 
+        <div className="right">
+          <img src="/img/payment.png" alt="" />
+        </div>
       </div>
     </div>
   )
