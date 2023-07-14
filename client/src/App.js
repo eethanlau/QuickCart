b267597fc361ae87d841af4feef89a7f36967b19
eethanlau <ethan.lau151@gmail.com>
2023-07-13 22:26:31 -0400

    finish navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,51 @@
-import logo from './logo.svg';
+import { Children } from "react";
 import './App.css';
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
+  Outlet,
 } from "react-router-dom";
-import Home from './pages/Home/Home';
-import Products from './pages/Products/Products';
-import Product from './pages/Product/Product';
 
+import Footer from "./components/Footer/Footer"
+import Navbar from "./components/Navbar/Navbar"
+import Home from './pages/Home/Home';
+import Product from './pages/Product/Product';
+import Products from './pages/Products/Products';
+
+// Import scss file
+import "./app.scss";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home/>
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/products/:id",
+        element: <Products />
+      },
+      {
+        path: "/product/:id",
+        element: <Product />
+      },
+    ],
+    
   },
-  {
-    path: "/products/:id",
-    element: <Products/>
-  },
-  {
-    path: "/product/:id",
-    element: <Product/>
-  }
-])
+]);
 
 function App() {
   return (
