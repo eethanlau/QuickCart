e00f02abc8e5e966a842c1d3af8ef2a14c801b50
eethanlau <ethan.lau151@gmail.com>
2023-07-18 23:38:09 -0400

    Categories component design and  complete

---
+++ client/src/components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Contacts.scss"
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <div className="wrapper">
+        <span>CONTACT US!</span>
+      </div>
+    </div>
+  )
+}
+
+export default Contact
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts';
 import Slider from '../../components/Slider/Slider'
 import './Home.scss';
+import Categories from '../../components/Categories/Categories';
 
 const Home = () => {
   return (
     <div className="home">
       <Slider/>
       <FeaturedProducts type="featured"/>
+      <Categories/>
       <FeaturedProducts type="trending"/>
     </div>
   );
